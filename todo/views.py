0532edc879e62010800c8797ee6d702e673f5131
Vraj <virajhiwale05@gmail.com>
2025-03-05 08:04:21 +0530

    second commit

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Todo
 # Define the todo_list view function
 def todo_list(request):
     # Ensure this line is indented properly
     return render(request, 'todo/index.html')
+
+    def create_todo(request):
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+            Todo.objects.create(title=title, description=description)
+
+            return redirect('todo_list')
+            return render(request, 'todo/create_todo.html')
