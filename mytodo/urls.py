3215c4950a9119f7b3acb1d5468b42fa65bfb7f1
Vraj <virajhiwale05@gmail.com>
2025-03-06 16:27:17 +0530

    third commit

--- mytodo/urls.py
+++ mytodo/urls.py
@@ -20,6 +20,7 @@ from todo import views
 
 urlpatterns = [
     path('todo/', views.todo_list, name="todo_list"),
-    path('todo/create', views.create_todo, name='create_list'),
+    path('todo/create', views.create_todo, name='create_todo'),
+    path('todo/complete/<int:todo_id>', views.complete_todo, name='complete_todo'),
     path('admin/', admin.site.urls),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render, redirect
 from .models import Todo
 # Define the todo_list view function
 def todo_list(request):
+    todos = Todo.objects.all()
     # Ensure this line is indented properly
-    return render(request, 'todo/index.html')
+    return render(request, 'todo/index.html',{'todos':todos})
 
-    def create_todo(request):
-        if request.method == 'POST':
-            title = request.POST.get('title')
-            description = request.POST.get('description')
-            Todo.objects.create(title=title, description=description)
 
-            return redirect('todo_list')
-            return render(request, 'todo/create_todo.html')
+
+def create_todo(request):
+    if request.method == 'POST':
+        # Handle form submission
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Todo.objects.create(title=title, description=description)
+        return redirect('todo_list')  # Redirect to the todo list page
+    return render(request, 'todo/create_todo.html')
+
+    def complete_todo(request,todo_id):
+        todo = Todo.objects.get(id=todo_id)
+        todo.complete = True
+        todo.save()
+        return render('todo_list')
+
